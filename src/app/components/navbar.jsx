6825920a87040f196de9401c4f8f2927b8596ba4
onur han hacimustafaoglu <onurhanhacimustafaoglu@gmail.com>
2025-07-22 15:27:52 +0300

    first commit

---
+++ src/app/components/navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <>
+            <div className='bg-sidebar  w-full flex'>
+                <div>
+            <img src="navbar-icon.svg" alt="" />
+                </div>
+                <p></p>
+            </div>
+        </>
+
+    )
+}
