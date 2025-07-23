d127d0abfffae871b5d832e0611ffb1e145dec0e
onur han hacimustafaoglu <onurhanhacimustafaoglu@gmail.com>
2025-07-23 14:45:33 +0300

    fixex

--- src/app/components/invoices.jsx
+++ src/app/components/invoices.jsx
@@ -12,7 +12,7 @@ import {
 } from "@/components/ui/dialog";
 import { Label } from '@/components/ui/label';
 import { Input } from '@/components/ui/input';
-import DatePicker from './datePicker';
+import DatePicker from './datepicker';
 
 export default function Invoices() {
     return (
